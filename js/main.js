8b52807af382332437d300741bbda997658ac2f7
hrhr0616 <teuk608@naver.com>
2023-03-30 15:31:07 +0900

    '공통모듈분리'

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function() {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function() {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function() {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -48,7 +31,7 @@ toTopEl.addEventListener('click', function() {
   gsap.to(window, .7, {
     scrollTo: 0
   })
-})
+});
 
 const fadeEls = document.querySelectorAll('.fade-in');
 fadeEls.forEach(function(fadeEl, index) {
@@ -139,4 +122,4 @@ spyEls.forEach(function(spyEl) {
   })
   .setClassToggle(spyEl, 'show')
   .addTo(new ScrollMagic.Controller());
-})
+});
